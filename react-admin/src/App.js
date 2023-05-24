deabf9d35934dc273065eb8c53bf968649b20042
Emre <emreozgoz@gmail.com>
2023-05-24 11:24:36 +0300

    feat: add calendar

--- react-admin/src/App.js
+++ react-admin/src/App.js
@@ -8,12 +8,12 @@ import Team from "./scenes/team";
 import Contacts from "./scenes/contacts";
 import Invoices from "./scenes/invoices";
 import Form from "./scenes/form";
+import Calendar from "./scenes/calendar";
 // import Bar from "./scenes/bar";
 // import Line from "./scenes/line";
 // import Pie from "./scenes/pie";
 // import FAQ from "./scenes/faq";
 // import Geography from "./scenes/geography";
-// import { Calendar } from "./scenes/calendar";
 function App() {
   const [theme, colorMode] = useMode();
 
@@ -31,13 +31,13 @@ function App() {
               <Route path="/contacts" element={<Contacts />} />
               <Route path="/invoices" element={<Invoices />} />
               <Route path="/form" element={<Form />} />
-              {/*
+              <Route path="/calendar" element={<Calendar />} />
+              {/* /*
               // <Route path="/bar" element={<Bar />} />
               // <Route path="/pie" element={<Pie />} />
               // <Route path="/line" element={<Line />} />
               // <Route path="/faq" element={<FAQ />} />
-              // <Route path="/geography" element={<Geography />} />
-              // <Route path="/calendar" element={<Calendar />} /> */}
+              // <Route path="/geography" element={<Geography />} /> */}
             </Routes>
           </main>
         </div>
